f949ee2354857a077be15f1a2e443fa4632d934e
Andrew Froiland <afroiland@Andrews-MacBook-Pro-2.local>
2016-11-25 16:11:05 -0600

    adding database.sql file

---
+++ database.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE employees (
+first_name VARCHAR(50) NOT NULL,
+last_name VARCHAR(50) NOT NULL,
+emp_id INTEGER NOT NULL,
+job_title VARCHAR(50) NOT NULL,
+annual_salary INTEGER NOT NULL
+);
