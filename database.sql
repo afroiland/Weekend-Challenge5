a1f5af73bff79f9ef5dec3153b497295ef55b9a7
Andrew Froiland <afroiland@Andrews-MacBook-Pro-2.local>
2016-11-25 16:28:17 -0600

    employee table

--- database.sql
+++ database.sql
@@ -6,3 +6,8 @@ emp_id INTEGER NOT NULL,
 job_title VARCHAR(50) NOT NULL,
 annual_salary INTEGER NOT NULL
 );
+
+INSERT INTO employees (first_name, last_name, emp_id, job_title, annual_salary)
+VALUES ('Ed', 'Smith', '3333', 'CEO', '120000'),
+('Ted', 'Smith', '3334', 'CFO', '120000'),
+('Fred', 'Smith', '3335', 'CTO', '120000');
